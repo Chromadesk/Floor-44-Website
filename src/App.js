a4d083106e1284952cf9b637d19b8b3fde40a6a7
Chromadesk <yisraeleliyah@gmail.com>
2024-05-01 15:38:51 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
+function NavBar() {
+
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <NavBar></NavBar>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
